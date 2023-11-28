9eaf92cbfc8cfed2bc6110b43e81cd50517a3f19
Haitao-H <98918272+Haitao-H@users.noreply.github.com>
2023-11-28 17:33:02 -0500

    Update media.js
    add style for collaps

--- public/media.js
+++ public/media.js
@@ -1,84 +1,49 @@
-const boxSelector = document.querySelectorAll('.box-selector');
+<!DOCTYPE html>
+<html>
+
+<head>
+    <%- include('partials/head') %>
+        <script src="/media.js" defer></script>
+        <link rel="stylesheet" href="/css/media.css">
+</head>
+
+<body>
 
 
-fetch('/media/category')
-    .then((res) => res.json())
-    .then((data) => {
-        let categoryList = data.data;
-
-        for (let i = 0; i < categoryList.length; i++) {
-            let endpoint = '/media/' + categoryList[i];
-
-            fetch(endpoint)
-                .then((res) => res.json())
-                .then((data) => {
-                    // console.log(data.data)
-                    const list = data.data;
-                    let html = ``;
-                    list.forEach(element => {
-                        html += `
-                <label><input type='checkbox' class='common-selector ${categoryList[i]}' value='${element}'>${element}</label>
-                <br>`
-                    });
-                    boxSelector[i].innerHTML = html;
-                })
-        }
-
-    });
-
-filterData();
-
-function filterData() {
-    const species = getFilter('species');
-    const year = getFilter('year');
-    // console.log(species)
-    // console.log(year)
-
-    $.ajax({
-        url: '/media/result',
-        method: 'GET',
-        data: {
-            species,
-            year
-        },
-        success: function (result) {
-            let html = ``;
-            if (result.message == 'success') {
-                let data = result.data;
-                // console.log(data);
-                data.forEach((record) => {
-
-                    html += `<div class="single-record">${record._id}`
-                    if(record.isCrab){
-                        html += `<h4>Crab</h4>`
-                    }
-                    if(record.isParrot){
-                        html += `<h4>Parrot</h4>`
-                    }
-                    html+= `<h4>${record.year}</h4></div>`
-                    // console.log(record)
-                })
-
-            }
-            document.querySelector('.content-container').innerHTML = html;
-        }
-    })
+    <%- include('partials/topnav') %>
 
 
-}
+        <div class="main-container">
 
-function getFilter(className) {
-    let filter = [];
-    let commonSelector = document.querySelectorAll(`.common-selector.${className}:checked`);
-    commonSelector.forEach((e) => {
-        filter.push(e.value);
-    })
-    return filter;
-}
+            <div class="filter-container">
 
-document.addEventListener('click', function (e) {
-    if (e.target.classList.contains('common-selector')) {
-        filterData();
-    }
-})
+                <div class="list-group species">
+                    <h3 class="collapsible">Species</h3>
+                    <div class="box-selector"></div>
+                </div>
 
+                <div class="list-group year">
+                    <h3 class="collapsible">Year</h3>
+                    <div class="box-selector"></div>
+                </div>
+            </div>
+
+            <div class="content-container">
+                
+            </div>
+
+            <a href="/media/share"><div id="share-btn">share</div></a>
+
+        </div>
+
+        <%- include('partials/footer') %>
+
+</body>
+
+<script>
+
+
+
+</script>
+
+</html>
