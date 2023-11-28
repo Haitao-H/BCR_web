5aa20dae38c6c90bd44f5e18e84ce643fdcc99cc
Huajie-1 <121126880+Huajie-1@users.noreply.github.com>
2023-11-28 17:29:40 -0500

    Update loginRoutes.js

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,20 +1,59 @@
 const express = require('express')
 const router = express.Router();
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const dataModel = require('../dataModel');
 
 router.get('/login', (req, res) => {
     res.render('login', { title: 'login' })
 })
 
+router.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
-router.post('/home', (req,res) => {
-    const userData={
-        email:req.body.email,
-        password:req.body.password
+router.post('/home', async (req,res) => {
+    const { email, password } = req.body;
+
+    try {
+        // check the user in database
+        const user = await dataModel.User.findOne({ email: email });
+
+        if (!user) {
+            res.status(401).send('no account match');
+            //console.log('no account match:'+email);
+            return;
+        }
+        console.log("account match")
+
+        // check password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            // user already log in
+            req.session.user = user;
+            //res.send('log in success');
+            res.render('home', { title: 'Home' })
+        } else {
+            console.log('Password does not match for email: ' + email);
+            res.status(401).send('password wrong');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('log in failed');
     }
-    console.log(req.body);
-    res.render('home', { title: 'Home' })
-    
+
+});
+
+router.get('/check-login', (req, res) => {
+    if (req.session.user) {
+        res.send('user already log in');
+    } else {
+        res.send('user does not log in');
+    }
 });
 
 module.exports = router;
