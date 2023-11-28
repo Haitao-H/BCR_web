131ffc67b592febcd2f679b9d6b348644f025a0d
Huajie-1 <121126880+Huajie-1@users.noreply.github.com>
2023-11-28 17:37:38 -0500

    Update permission.js

---
+++ routes/permission.js
@@ -0,0 +1,55 @@
+const roles = {
+    PUBLIC: 'public',
+    REGISTERED: 'registered',
+    BCR_MEMBER: 'bcr_member',
+    ADMINISTRATOR: 'administrator',
+  };
+  
+  const permissions = {
+    SEARCH: 'search',
+    WATCH: 'watch',
+    UPLOAD: 'upload',
+    EDIT: 'edit',
+    DELETE: 'delete',
+  };
+  
+  function checkPermission(permission) {
+    return (req, res, next) => {
+      const user = req.session.user; // Assuming user information is stored in the session
+  
+      if (!user) {
+        return res.status(401).send('Unauthorized');
+      }
+  
+      // Check if the user has the required permission
+      if (user.permissions.includes(permission)) {
+        next();
+      } else {
+        res.status(403).send('Forbidden');
+      }
+    };
+  }
+  
+  // Example routes with middleware for authorization
+  app.get('/search', checkPermission(permissions.SEARCH), (req, res) => {
+    res.send('Performing search');
+  });
+  
+  app.get('/watch', checkPermission(permissions.WATCH), (req, res) => {
+    res.send('Watching content');
+  });
+  
+  app.post('/upload', checkPermission(permissions.UPLOAD), (req, res) => {
+    res.send('Uploading content');
+  });
+  
+  app.put('/edit', checkPermission(permissions.EDIT), (req, res) => {
+    res.send('Editing content');
+  });
+  
+  app.delete('/delete', checkPermission(permissions.DELETE), (req, res) => {
+    res.send('Deleting content');
+  });
+  
+
+  
