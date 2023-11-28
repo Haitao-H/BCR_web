facff225a1d0e8572a5ea0e9d6c7b0f2df5c26d4
Huajie-1 <121126880+Huajie-1@users.noreply.github.com>
2023-11-28 17:36:58 -0500

    Update signupRoutes.js

--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,23 +1,42 @@
 const express = require('express')
 const router = express.Router();
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const dataModel = require('../dataModel');
 
 router.get('/signup', (req, res) => {
     res.render('signup', { title: 'signup' })
 })
 
-router.post('/signup',(req, res) => {
-    console.log(req.body);
-    const userData={
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-        repassword:req.body.repassword
-    }
+router.post('/signup', async(req, res) => {
+    const { name, email, password } = req.body;
+    try{
+        const existingUser = await dataModel.User.findOne({email});
 
-    console.log(userData);
-    res.render('login', { title: 'Login' })
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email address is already in use' });
+          }
+        
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = new dataModel.User({
+        name,
+        email,
+        password: hashedPassword
+        });
+        console.log(user);
+
+        user.save()
+        .then(() => {
+        console.log('User registered successfully');
+        res.redirect('/login');
+        })
+    }catch(err) {
+        console.error(err);
+        console.log('Registration failed');
+        res.render('signup', {title:'Signup'});
+    }
+    //res.render('login', { title: 'Login' })
 });
 
-
 module.exports = router;
+
