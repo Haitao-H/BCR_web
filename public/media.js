3f56eec0877df398384b1fed53259fcef873f346
Haitao-H <98918272+Haitao-H@users.noreply.github.com>
2023-11-28 17:44:26 -0500

    Update media.js
    redo

--- public/media.js
+++ public/media.js
@@ -1,49 +1,108 @@
-<!DOCTYPE html>
-<html>
+// filter function
 
-<head>
-    <%- include('partials/head') %>
-        <script src="/media.js" defer></script>
-        <link rel="stylesheet" href="/css/media.css">
-</head>
-
-<body>
+const boxSelector = document.querySelectorAll('.box-selector');
 
 
-    <%- include('partials/topnav') %>
+fetch('/media/category')
+    .then((res) => res.json())
+    .then((data) => {
+        let categoryList = data.data;
+
+        for (let i = 0; i < categoryList.length; i++) {
+            let endpoint = '/media/' + categoryList[i];
+
+            fetch(endpoint)
+                .then((res) => res.json())
+                .then((data) => {
+                    // console.log(data.data)
+                    const list = data.data;
+                    let html = ``;
+                    list.forEach(element => {
+                        html += `
+                <label><input type='checkbox' class='common-selector ${categoryList[i]}' value='${element}'>${element}</label>
+                <br>`
+                    });
+                    boxSelector[i].innerHTML = html;
+                })
+        }
+
+    });
+
+filterData();
+
+function filterData() {
+    const species = getFilter('species');
+    const year = getFilter('year');
+    // console.log(species)
+    // console.log(year)
 
 
-        <div class="main-container">
+    // show all the result for the resources
+    $.ajax({
+        url: '/media/result',
+        method: 'GET',
+        data: {
+            species,
+            year
+        },
+        success: function (result) {
+            let html = ``;
+            if (result.message == 'success') {
+                let data = result.data;
+                // console.log(data);
+                data.forEach((record) => {
 
-            <div class="filter-container">
+                    html += `<div class="single-record">${record._id}`
+                    
+                    html+= `<h4>${record.species}</h4>
+                            <h4>${record.year}</h4>
+                            <h4>${record.location}</h4>
+                            <h4>${record.media}</h4>
+                            </div>`
+                    // console.log(record)
+                })
 
-                <div class="list-group species">
-                    <h3 class="collapsible">Species</h3>
-                    <div class="box-selector"></div>
-                </div>
-
-                <div class="list-group year">
-                    <h3 class="collapsible">Year</h3>
-                    <div class="box-selector"></div>
-                </div>
-            </div>
-
-            <div class="content-container">
-                
-            </div>
-
-            <a href="/media/share"><div id="share-btn">share</div></a>
-
-        </div>
-
-        <%- include('partials/footer') %>
-
-</body>
-
-<script>
+            }
+            document.querySelector('.content-container').innerHTML = html;
+        }
+    })
 
 
+}
 
-</script>
+function getFilter(className) {
+    let filter = [];
+    let commonSelector = document.querySelectorAll(`.common-selector.${className}:checked`);
+    commonSelector.forEach((e) => {
+        filter.push(e.value);
+    })
+    return filter;
+}
 
-</html>
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('common-selector')) {
+        filterData();
+    }
+})
+
+
+
+
+
+
+// collapsible content
+
+let collaps = document.getElementsByClassName("collapsible");
+
+for(let i = 0; i<collaps.length; i++){
+    collaps[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        let selectors = this.nextElementSibling;
+
+        if (selectors.style.maxHeight){
+            selectors.style.maxHeight = null;
+          } else {
+            selectors.style.maxHeight = selectors.scrollHeight + "px";
+          }
+    })
+}
