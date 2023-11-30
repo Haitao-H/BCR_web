d005c644eb790e7a18d1a7c43a42347c17345492
tao2 <hu819495707@gmail.com>
2023-11-30 15:07:58 -0500

    improve and organize the auth files

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcrypt');
 const session = require('express-session');
 const dataModel = require('../dataModel');
 
+
+// login logic
 router.get('/login', (req, res) => {
     if (req.session.user) {
         res.redirect('/home');
@@ -53,6 +55,7 @@ router.post('/home', async (req, res) => {
 });
 
 
+// register logic
 router.get('/register', (req, res) => {
     res.render('register', { title: 'register' })
 })
@@ -84,6 +87,44 @@ router.post('/register', async (req, res) => {
         console.error(err);
         console.log('Registration failed');
         res.render('register', { title: 'register' });
+    }
+});
+
+
+// reset password logic
+router.get('/resetPassword', (req, res) => {
+    res.render('resetPassword', { title: 'Reset Password' });
+});
+
+
+router.post('/', async (req, res) => {
+    const { email, password, newPassword } = req.body;
+
+    try {
+        // find email
+        const user = await dataModel.User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        //
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Incorrect password' });
+        }
+
+        // update password
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+
+        res.status(200).send('Password reset successful');
+        res.redirect('/login');
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
 
