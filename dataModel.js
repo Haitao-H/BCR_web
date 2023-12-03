90d76641b3a715b560c6de5130f5c6bd7b8f1f03
tao2 <hu819495707@gmail.com>
2023-12-03 16:55:09 -0500

    updata

--- dataModel.js
+++ dataModel.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String,  unique: true, required: true },
     password: { type: String, required: true },
-    level:{ type: internal }
+    level:{ type: Number }
 })
 
 
