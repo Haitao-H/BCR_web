6b07b4dc5c1f087a5dd51376c54695426e3921b4
huajie-1 <denghuajie0416@gmail.com>
2023-12-03 16:52:50 -0500

    add level in user

--- dataModel.js
+++ dataModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const internal = require('stream');
 
 
 const bcrSchema = new mongoose.Schema({
@@ -54,7 +55,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String,  unique: true, required: true },
     password: { type: String, required: true },
-    level:{ type: String }
+    level:{ type: internal }
 })
 
 
